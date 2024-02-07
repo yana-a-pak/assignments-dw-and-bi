cd2aebdf67c5f2dfd5c711ca3e2847946ad8f908
yana-a-pak <157728874+yana-a-pak@users.noreply.github.com>
2024-02-07 14:39:52 +0000

    Revised create tables

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -74,44 +74,6 @@ table_create_events = """
     )
 """
 
-
-# Insert data
-actors_insert = ("""
-INSERT INTO actors (actor_id, actor_login, actor_display_login, actor_gravatar_id, actor_url, actor_avatar_url)
-VALUES (%s, %s, %s, %s, %s, %s)
-ON CONFLICT (actor_id)
-DO NOTHING
-""")
-
-repo_insert = ("""
-INSERT INTO repo (repo_id, repo_name, repo_url)
-VALUES (%s, %s, %s)
-ON CONFLICT (repo_id)
-DO NOTHING
-""")
-
-payload_insert = ("""
-INSERT INTO payload (push_id, size, distinct_size, ref, head, before, commits)
-VALUES (%s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (push_id)
-DO NOTHING
-""")
-
-org_insert = ("""
-INSERT INTO org (org_id, org_login, org_gravatar_id, org_url, org_avatar_url)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (org_id)
-DO NOTHING
-""")
-
-events_insert = ("""
-INSERT INTO events (event_id, event_type, actor_id, repo_id, payload_action, payload_push_id, public, created_at, org_id, event_time)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (event_id)
-DO NOTHING
-""")
-
-
 # Queries
 create_table_queries = [
     table_create_actors,
